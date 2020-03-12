5dda3278eef681117bd182289635ba93818d25e7
Wladimir Oliveira <wlad500@hotmail.com>
2020-03-12 10:04:28 -0300

    Funções

--- bundle.js
+++ bundle.js
@@ -1,19 +1,48 @@
 "use strict";
 
-var nomeVariavel = "João"; // Objeto
+// Operações com array em ES6+
+var array = [1, 3, 4, 5, 8, 9]; //Percorre todos os itens do vetor e executa determinada ação para ele como retorno
 
-var usuario = {
-  nome: 'Jesus',
-  idade: 98
-};
-usuario.nome = 'Maria';
-console.log(usuario);
-var meuVetor = [1, 2, 3, 4, 5];
-meuVetor[0] = 90;
-meuVetor[2] = 93;
-console.log(meuVetor); //Variável do tipo let
+var novoArray = array.map(function (item, index) {
+  return item + index;
+});
+console.log(novoArray); //Reduce: consumir todo o vetor e transformar em uma única informação (geralmente número);
 
-var valor = 3;
-var nome = 'Claudia Let';
-var altura = 1.86;
-var contemValor = true;
+var soma = array.reduce(function (total, proximoValor) {
+  return total + proximoValor;
+});
+console.log(soma);
+/* 
+    Execução:
+    total: 0 próximo: 1
+    total: 1 próximo: 3
+    total: 4 próximo: 4
+*/
+//Filter: filtrar somente os itens que queremos
+
+var filterPares = array.filter(function (item) {
+  /* if(item%2==0){
+      return true;
+  }else{
+      return false;
+  }
+  */
+  return item % 2 === 0;
+});
+var novosUsuarios = array.filter(function (usuario) {
+  return usuario.idade >= 15;
+});
+console.log(filterPares); //Find
+
+var find = array.find(function (item) {
+  return item === 4;
+});
+
+if (find) {
+  console.log(find);
+} //forEach
+
+
+novosUsuarios.forEach(function (item, index) {
+  console.log(item);
+});
--- main.js
+++ main.js
@@ -1,21 +1,54 @@
-const nomeVariavel="João";
+// Operações com array em ES6+
+const array = [1,3,4,5,8,9];
 
-// Objeto
-const usuario = { nome: 'Jesus', idade: 98};
+//Percorre todos os itens do vetor e executa determinada ação para ele como retorno
+const novoArray=array.map(function(item,index){
+    return item+index;
+});
+console.log(novoArray);
+//Reduce: consumir todo o vetor e transformar em uma única informação (geralmente número);
+const soma = array.reduce(function(total,proximoValor){
+    return total+proximoValor;
+});
 
-usuario.nome='Maria';
+console.log(soma);
+/* 
+    Execução:
+    total: 0 próximo: 1
+    total: 1 próximo: 3
+    total: 4 próximo: 4
+*/
 
-console.log(usuario);
+//Filter: filtrar somente os itens que queremos
+const filterPares = array.filter(function(item){
+    /* if(item%2==0){
+        return true;
+    }else{
+        return false;
+    }
+    */
+    return item % 2 ===0;
+});
 
-const meuVetor = [1,2,3,4,5];
-meuVetor[0]=90;
-meuVetor[2]=93;
+const novosUsuarios=array.filter(function(usuario){
+    return usuario.idade>=15;
+})
 
-console.log(meuVetor);
+console.log(filterPares);
 
-//Variável do tipo let
-let valor = 3;
-let nome = 'Claudia Let';
-let altura = 1.86;
-let contemValor=true;
+
+//Find
+
+const find = array.find(function(item){
+    return item ===4;
+});
+
+if(find){
+    console.log(find);
+}
+
+//forEach
+novosUsuarios.forEach(function(item,index){
+    console.log(item);
+});
 
