7435aa50b6ecdf872d7003b6786e49a6b055fe53
Wladimir Oliveira <wlad500@hotmail.com>
2020-03-12 10:50:30 -0300

    rest e mais aula

--- main.js
+++ main.js
@@ -7,14 +7,27 @@ const usuario = {
     }
 };
 
-console.log(usuario);
-/*
-let nome = usuario.nome;
-let idade = usuario.idade;
-let cidade = usuario.cidade;
+const { nome, ...resto}=usuario;
 
-console.log(nome, idade, cidade);
-*/
+console.log(nome);
+console.log(resto);
 
-const{nome, idade, endereco: {cidade}} = usuario;
-console.log(nome, idade, cidade);
+const array = [1,2,3,4];
+const array2 = [5,6,7];
+
+const [primeiro, segundo, ...restante] = array;
+console.log(primeiro,segundo);
+console.log(restante);
+
+function soma(...parametros){
+    console.log(parametros);
+    return parametros.reduce((total,next) => total + next);
+}
+
+console.log(soma(1,3,6,9,1,11));
+
+const novoArray = [...array, ...array2];
+console.log(novoArray);
+
+const usuario2 = { ...usuario, nome: 'Maria Carolina'};//Pega todo o conteúdo de usuario e sobrescreve o nome
+console.log(usuario2);
