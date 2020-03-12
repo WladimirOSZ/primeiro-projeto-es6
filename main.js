2106f34fe946b6cb380b3b6a5018a7c1fda4d182
Wladimir Oliveira <wlad500@hotmail.com>
2020-03-12 10:55:47 -0300

    Formas mais fáceis de printar

--- main.js
+++ main.js
@@ -1,33 +1,11 @@
-const usuario = {
-    nome: 'Jordan',
-    idade: 24,
-    endereco: {
-        cidade: 'Ituiutaba',
-        estado: 'MG'
-    }
-};
+//Template Literals
+let fullName = 'Uchiha Sasuke';
+let age = 11;
 
-const { nome, ...resto}=usuario;
+//"Meu nome é uchiha sasuke, tenho 11 anos."
+console.log("Meu nome é "+ fullName + ". E tenho "+ age + "anos");
 
-console.log(nome);
-console.log(resto);
+console.log(`Meu nome é ${fullName}. E tenho ${age} anos`);
 
-const array = [1,2,3,4];
-const array2 = [5,6,7];
-
-const [primeiro, segundo, ...restante] = array;
-console.log(primeiro,segundo);
-console.log(restante);
-
-function soma(...parametros){
-    console.log(parametros);
-    return parametros.reduce((total,next) => total + next);
-}
-
-console.log(soma(1,3,6,9,1,11));
-
-const novoArray = [...array, ...array2];
-console.log(novoArray);
-
-const usuario2 = { ...usuario, nome: 'Maria Carolina'};//Pega todo o conteúdo de usuario e sobrescreve o nome
-console.log(usuario2);
+var mensagem = `Meu nome é ${fullName}. E tenho ${age} anos`;
+console.log(mensagem);
